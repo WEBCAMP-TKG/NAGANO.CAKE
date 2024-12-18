17f14f3d64132676ed7f85559284512dbe9da28a
EC2 Default User <ec2-user@ip-172-31-45-31.ap-northeast-1.compute.internal>
2024-12-18 04:52:40 +0000

    cart_item&order

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,11 @@
 class CartItem < ApplicationRecord
+  belongs_to :customer
+  belongs_to :item
+  validates :item_id, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+
+  def add_tax_price
+    (self.item.price * 1.10).round
+   end
+
 end
