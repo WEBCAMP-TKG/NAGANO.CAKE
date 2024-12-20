7bfe11878b4c69cb6d42896933611cc2f7598dfc
EC2 Default User <ec2-user@ip-172-31-45-31.ap-northeast-1.compute.internal>
2024-12-20 06:29:27 +0000

    12.20

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,7 +2,7 @@ class Public::HomesController < ApplicationController
 
   def top
     @genres = Genre.all
-    @items = Item.where(is_active: true).order(created_at: :desc).limit(4)
+    @items = Item.where(is_sell_status: true).order(created_at: :desc).limit(4)
   end
 
 # GET	/about
