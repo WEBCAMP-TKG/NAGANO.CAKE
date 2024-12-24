7f8b4b092e488d05b61675e89df97f9005115842
EC2 Default User <ec2-user@ip-172-31-45-31.ap-northeast-1.compute.internal>
2024-12-24 04:29:38 +0000

    finish

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,23 +1,38 @@
 class Admin::OrderDetailsController < ApplicationController
     before_action :authenticate_admin!
-
+  
     def update
-        @order_detail = OrderDetail.find(params[:id])
-        @order_detail.update(order_detail_params)
-        case @order_detail.making_status
-        when "製作中"
-        @order_detail.order.update(status: "製作中")
-        when "製作完了"
-        if @order_detail.order.order_details.all?{|order_detail| order_detail.making_status == "製作完了"}
-        @order_detail.order.update(status: "発送準備中")
+      @order_detail = OrderDetail.find(params[:id])
+      @order = @order_detail.order  # 関連する注文を取得
+  
+      # 製作ステータスを更新
+      if @order_detail.update(order_detail_params)
+        
+        # 1. 製作ステータスが「製作中」に変更された場合、注文ステータスを「製作中」に変更
+        if @order_detail.making_status == "making_production"
+          # 注文詳細の1つでも製作ステータスが「製作中」であれば注文ステータスを「製作中」に変更
+          @order.update(status: "in_production")
         end
+  
+        # 2. 製作ステータスが「製作完了」に変更された場合、すべての注文詳細が「製作完了」であれば注文ステータスを「発送準備中」に変更
+        if @order_detail.making_status == "finished"
+          # すべての注文詳細の製作ステータスが「製作完了」の場合
+          if @order.order_details.all? { |detail| detail.making_status == "finished" }
+            @order.update(status: "prep_to_ship")
+          end
         end
-        redirect_to admin_order_path(@order_detail.order)
+  
+        flash[:notice] = "注文詳細の更新に成功しました。"
+      else
+        flash[:alert] = "注文詳細の更新に失敗しました。"
+      end
+  
+      redirect_to admin_order_path(@order)
     end
-
+  
     private
-
+  
     def order_detail_params
-        params.require(:order_detail).permit(:making_status)
+      params.require(:order_detail).permit(:making_status)
     end
-end
+  end
